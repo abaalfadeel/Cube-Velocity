c691380a824d61afc38baf4621ea904e748d146c
abaalfadeel <abaalfadeel1@gmail.com>
2025-12-09 12:58:18 +0300

    Update ads.js

--- scripts/ads.js
+++ scripts/ads.js
@@ -1,20 +1,5 @@
-// ads.js
-const Ads = (function(){
-  function showInterstitial(callback){
-    // محاكاة: استدعاء حوار ثم callback
-    setTimeout(()=> {
-      // هنا تضع كود الشبكة الإعلانية الفعلي
-      alert('إعلان بيني (محاكاة)');
-      if(callback) callback();
-    }, 200);
-  }
-
-  function showRewarded(callback){
-    setTimeout(()=> {
-      alert('إعلان مكافئ (محاكاة)');
-      if(callback) callback();
-    }, 200);
-  }
-
-  return { showInterstitial, showRewarded };
-})();
+const Ads = {
+    showInterstitial(cb){
+        setTimeout(cb, 300); // بدون إعلانات حقيقية
+    }
+};
